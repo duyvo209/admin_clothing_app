c39ab8647d615c4afec2cdc6d43b665fae3563e6
Duy Vo <voduy200999@gmail.com>
2020-12-23 21:13:43 +0700

    up

--- index.js
+++ index.js
@@ -29,66 +29,52 @@ function addData() {
     });  
 }
 
-// function updateData(docID) {
-//     var inputID = Number(document.getElementById("id").value);
-//     var inputProductName = document.getElementById("tensanpham").value;
-//     var inputQuantity = Number(document.getElementById("soluong").value);
-//     var inputType = document.getElementById("loai").value;
-//     var inputPrice = Number(document.getElementById("gia").value);
-//     var inputDesc = document.getElementById("mota").value;
-//     var inputImage = document.getElementById("anh").value;
-//     var arrayImage = inputImage.split(', ');
-    
-//     db.collection("products").doc(docID).update({
-//         id: inputID,
-//         name: inputProductName,
-//         quantity: inputQuantity,
-//         type: inputType,
-//         price: inputPrice,
-//         description: inputDesc,
-//         img: inputImage,
-//         image: arrayImage
-//     })
-//     .then(function() {
-//         console.log("Document successfully written!");
-//     })
-//     .catch(function(error) {
-//         console.error("Error writing document: ", error);
-//     });  
-// }
-
-// function deleteData() {
-//     var inputID = Number(document.getElementById("id").value);
-    
-//     db.collection("products").doc().detele()
-//     .then(function() {
-//         console.log("Document successfully written!");
-//     })
-//     .catch(function(error) {
-//         console.error("Error writing document: ", error);
-//     });  
-// }
-
 const getListUsers = document.querySelector("#getListUsers");
 
 db.collection("users").get().then(function(querySnapshot) {
-    querySnapshot.forEach(function(doc) {
-        getListUsers.innerHTML +=
-        "<td>" + doc.data().firstname + "</td><td>" + doc.data().lastname + "</td><td>" + doc.data().email + "</td>"
-    });
+   querySnapshot.forEach(function(doc) {
+       getListUsers.innerHTML +=
+       "<td>" + doc.data().firstname + "</td><td>" + doc.data().lastname + "</td><td>" + doc.data().email + "</td>"
+   });
 });
 // test.firestore.js
 
+function handleChageStatus(userId, thisSelect) {
+    console.log(userId)
+    const statusValue = thisSelect.value;
+    console.log(statusValue)
+}
+
+//zaconst updateStatusOrders = (userId, statusValue) => db.collection("orders").doc(userId).update(statusValue);
+
+
 const getListOrders = document.querySelector("#getListOrders");
+
+function updateDataIntoTable(data){
+    const statusName = ['Cancel','Ordered', 'Received']
+    getListOrders.innerHTML +=
+    "<td>" + data.userId + "</td><td>" + data.name + "</td><td>" 
+    + data.carts.map(item => item.product.name) + "</td><td>" 
+    + data.phone + "</td><td>" + data.address
+     + "</td><td>" + data.total + "</td><td>" 
+     + `<select key='${data.userId}' onchange="handleChageStatus('${data.userId}', this)" class="select-status">
+            <option ${data.status === 0 ? 'selected' : ''} value='0'>Cancel</option>
+            <option ${data.status === 1 ? 'selected' : ''} value='1'>Ordered</option>
+            <option ${data.status === 2 ? 'selected' : ''} value='2'>Received</option> 
+     </select>` + "</td>";
+
+}
+
 
 db.collection("orders").get().then(function(querySnapshot) {
     querySnapshot.forEach(function(doc) {
-        console.log(doc.data())
-        getListOrders.innerHTML +=
-         "<td>" + doc.data().name + "</td><td>" + doc.data().carts.product.name + "</td><td>" + doc.data().phone + "</td><td>" + doc.data().address + "</td><td>" + doc.data().total + "</td><td>" + doc.data().status + "</td>"
+        //console.log(doc.data())
+        updateDataIntoTable(doc.data())
+
     });
     console.log(getListOrders.innerHTML);
 });
+
 
 const updateForm = document.getElementById('update-form');
 
@@ -108,7 +94,7 @@ db.collection("products").orderBy('id').get().then(function(querySnapshot) {
         console.log(docProduct);
 
         getListProducts.innerHTML +=
-        "<?>"+ doc.data().id + "</?><td>" + doc.data().name + "</td><td>" + doc.data().price + "</td><td>" + doc.data().quantity + "</td><td>" + doc.data().type + "</td>" + `<td><button class='btn-update' data-id=${docProduct.id}>Sửa</button></td>` + `<td><button class='btn-delete' data-id=${docProduct.id}>Xóa</button></td>`
+        "<td>"+ doc.data().id + "</td><td>" + doc.data().name + "</td><td>" + doc.data().price + "</td><td>" + doc.data().quantity + "</td><td>" + doc.data().type + "</td>" + `<td><button class='btn-update' data-id=${docProduct.id}>Sửa</button></td>` + `<td><button class='btn-delete' data-id=${docProduct.id}>Xóa</button></td>`
         
         const btnUpdate = document.querySelectorAll('.btn-update');
         btnUpdate.forEach(btn => {
@@ -124,7 +110,6 @@ db.collection("products").orderBy('id').get().then(function(querySnapshot) {
                 updateForm['gia'].value = updateProduct.price;
                 updateFrom['mota'].value = updateProduct.desc;
                 updateForm['anh'].value = updateProduct.img;
-               
             })
         })
 
